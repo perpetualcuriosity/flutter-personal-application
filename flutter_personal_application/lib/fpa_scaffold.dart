d53c8104b91c1ead2bea9b10fca91aed68f1f14d
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-09 22:40:22 +0530

    trying out drawer navigation

--- flutter_personal_application/lib/fpa_scaffold.dart
+++ flutter_personal_application/lib/fpa_scaffold.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
-class Scaff extends StatefulWidget
-{
-    const Scaff({super.key});
-    @override
-    State<Scaff> createState() => _Scaff();
+
+class Scaff extends StatefulWidget {
+  const Scaff({super.key});
+  @override
+  State<Scaff> createState() => _Scaff();
 }
-class _Scaff extends State<Scaff>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Scaffold();
-    }
+
+class _Scaff extends State<Scaff> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test'),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              OutlinedButton(onPressed: () {}, child: Text('1')),
+              OutlinedButton(onPressed: () {}, child: Text('2')),
+              OutlinedButton(onPressed: () {}, child: Text('3')),
+              OutlinedButton(onPressed: () {}, child: Text('4')),
+              OutlinedButton(onPressed: () {}, child: Text('5'))
+            ],
+          ),
+        ),
+      ),
+      body: Column()
+    );
+  }
 }
