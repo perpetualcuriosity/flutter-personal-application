939053782c1de36e1e09294a991be710456a8463
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 00:42:56 +0530

    progress in home pages, nav buttons added, set state empty added

--- flutter_personal_application/lib/fpa_desktop_view_homepage.dart
+++ flutter_personal_application/lib/fpa_desktop_view_homepage.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
-class DesktopView extends StatefulWidget
-{
-    const DesktopView({super.key});
-    @override
-    State<DesktopView> createState() => _DesktopView();
+
+class DesktopView extends StatefulWidget {
+  const DesktopView({super.key});
+  @override
+  State<DesktopView> createState() => _DesktopView();
 }
-class _DesktopView extends State<DesktopView>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-      return(Scaffold(
+
+class _DesktopView extends State<DesktopView> {
+  @override
+  Widget build(BuildContext context) {
+    Widget wid = Container();
+    return (Scaffold(
         appBar: AppBar(
           title: Text('Jash Upadhyay'),
           actions: [
-            TextButton(onPressed: () {}, child: Text('Home')),
-            TextButton(onPressed: () {}, child: Text('Education')),
-            TextButton(onPressed: () {}, child: Text('Skills')),
-            TextButton(onPressed: () {}, child: Text('Projects')),
-            TextButton(onPressed: () {}, child: Text('Contact')),
+            TextButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Home')),
+            TextButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Education')),
+            TextButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Skills')),
+            TextButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Projects')),
+            TextButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text('Contact')),
           ],
         ),
-      ));
-    }
+        body: wid));
+  }
 }
--- flutter_personal_application/lib/fpa_mobile_view_homepage.dart
+++ flutter_personal_application/lib/fpa_mobile_view_homepage.dart
@@ -1,32 +1,116 @@
 import 'package:flutter/material.dart';
-class MobileView extends StatefulWidget
-{
-    const MobileView({super.key});
-    @override
-    State<MobileView> createState() => _MobileView();
+
+class MobileView extends StatefulWidget {
+  const MobileView({super.key});
+  @override
+  State<MobileView> createState() => _MobileView();
 }
-class _MobileView extends State<MobileView>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-      return(Scaffold(
-        appBar: AppBar(title: Text('Jash Upadhyay'),actions: [
-          IconButton(onPressed: () {
-              Scaffold.of(context).openDrawer();
-          }, icon: Icon(Icons.list))
-        ],),
+
+class _MobileView extends State<MobileView> {
+  Widget wid = Container();
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+        appBar: AppBar(title: Text('Jash Upadhyay')),
         drawer: Drawer(
+            child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
             children: [
-            TextButton(onPressed: () {}, child: Text('Home')),
-            TextButton(onPressed: () {}, child: Text('Education')),
-            TextButton(onPressed: () {}, child: Text('Skills')),
-            TextButton(onPressed: () {}, child: Text('Projects')),
-            TextButton(onPressed: () {}, child: Text('Contact')),
-            ]
+              Expanded(
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.home),
+                        SizedBox(width: 8), // Space between icon and text
+                        Text('Home'),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {});
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.school),
+                        SizedBox(width: 8),
+                        Text('Education'),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {});
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.star),
+                        SizedBox(width: 8),
+                        Text('Skills'),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {});
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.work),
+                        SizedBox(width: 8),
+                        Text('Projects'),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {});
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  title: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.contact_mail),
+                        SizedBox(width: 8),
+                        Text('Contact'),
+                      ],
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {});
+                  },
+                ),
+              ),
+            ],
           ),
-        ),
-      ));
-    }
+        )),
+        body: wid));
+  }
 }
