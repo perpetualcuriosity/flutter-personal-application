b5e35ff59a6cde8a09dfc609dc4725ebb45843e9
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 02:22:19 +0530

    global color edited

--- flutter_personal_application/lib/global_theme/global_theme.dart
+++ flutter_personal_application/lib/global_theme/global_theme.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 //frost
-List<Color> col = [Color(0xff000428),Color(0xff004e92)];
+List<Color> col = [Color(0xff020202),Color(0xff181818)];
 var stl = TextStyle(color: Colors.white,fontWeight: FontWeight.bold);
