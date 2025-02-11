98251a2331d686c91b3ea7dbc9f669488d8f4d6c
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 00:44:08 +0530

    minor refactor

--- flutter_personal_application/lib/fpa_matapp.dart
+++ flutter_personal_application/lib/fpa_matapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_personal_application/fpa_home.dart';
+import 'package:flutter_personal_application/fpa_view_control.dart';
 class MatApp extends StatelessWidget
 {
     const MatApp({super.key});
