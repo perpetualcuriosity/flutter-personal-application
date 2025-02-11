f36f6527ea8d85f7060870425b1f2681debb61a6
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 02:24:30 +0530

    final theme mostly

--- flutter_personal_application/lib/global_theme/global_theme.dart
+++ flutter_personal_application/lib/global_theme/global_theme.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 //frost
-List<Color> col = [Color(0xff020202),Color(0xff181818)];
+List<Color> col = [Color(0xff020202),Color(0xff020202)];
 var stl = TextStyle(color: Colors.white,fontWeight: FontWeight.bold);
