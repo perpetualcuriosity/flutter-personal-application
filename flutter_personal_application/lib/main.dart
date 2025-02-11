2e4265cd1e00758df343b948f08b1b6b4fb5f5b6
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 01:06:51 +0530

    directory cleanup

--- flutter_personal_application/lib/main.dart
+++ flutter_personal_application/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_personal_application/fpa_matapp.dart';
-void main()
-{
-    runApp(MatApp());
+import 'package:flutter_personal_application/fpa_view_control.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 }
