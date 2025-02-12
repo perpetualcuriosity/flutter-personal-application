71c30eb8a9f86c2d8244e2875b045fb2bf52cadd
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 21:51:23 +0530

    image added to home/about page

--- flutter_personal_application/lib/fpa_pages/page_home.dart
+++ flutter_personal_application/lib/fpa_pages/page_home.dart
@@ -2,36 +2,61 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
 import 'package:google_fonts/google_fonts.dart';
-class PageHome extends StatefulWidget
-{
-    const PageHome({super.key});
-    @override
-    State<PageHome> createState() => _PageHome();
+
+class PageHome extends StatefulWidget {
+  const PageHome({super.key});
+  @override
+  State<PageHome> createState() => _PageHome();
 }
-class _PageHome extends State<PageHome>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Container(
-          padding: EdgeInsets.all(13.0),
-          decoration: BoxDecoration(gradient: LinearGradient(colors:col)),
-            child: Center(child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text('Jash Upadhyay',style: GoogleFonts.sourceCodePro(color: col2,fontWeight: FontWeight.bold,fontSize: 100)),
-                  AnimatedTextKit(
-                  animatedTexts: [
-                    TyperAnimatedText('Solve.',speed: Duration(milliseconds: 50),textStyle: GoogleFonts.sourceCodePro(color: col2,fontWeight: FontWeight.bold,fontSize: 100)),
-                    TyperAnimatedText('Code.',speed: Duration(milliseconds: 50),textStyle: GoogleFonts.sourceCodePro(color: col2,fontWeight: FontWeight.bold,fontSize: 100)),
-                    TyperAnimatedText('Transform.',speed: Duration(milliseconds: 50),textStyle: GoogleFonts.sourceCodePro(color: col2,fontWeight: FontWeight.bold,fontSize: 100)),
-                    TyperAnimatedText('Flutter Developer',speed: Duration(milliseconds: 50),textStyle: GoogleFonts.sourceCodePro(color: col2,fontWeight: FontWeight.bold,fontSize: 100)),
-                  ],
-                  isRepeatingAnimation: false,
-                )
-                ],
-            ))
-        );
-    }
+
+class _PageHome extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(13.0),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: col),
+            image: DecorationImage(
+                image: AssetImage(
+                    'lib/assets/cbg.jpg'))),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text('Jash Upadhyay',
+                style: GoogleFonts.sourceCodePro(
+                    color: col2, fontWeight: FontWeight.bold, fontSize: 100)),
+            AnimatedTextKit(
+              animatedTexts: [
+                TyperAnimatedText('Solve.',
+                    speed: Duration(milliseconds: 50),
+                    textStyle: GoogleFonts.sourceCodePro(
+                        color: col2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100)),
+                TyperAnimatedText('Code.',
+                    speed: Duration(milliseconds: 50),
+                    textStyle: GoogleFonts.sourceCodePro(
+                        color: col2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100)),
+                TyperAnimatedText('Transform.',
+                    speed: Duration(milliseconds: 50),
+                    textStyle: GoogleFonts.sourceCodePro(
+                        color: col2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100)),
+                TyperAnimatedText('Flutter Developer',
+                    speed: Duration(milliseconds: 50),
+                    textStyle: GoogleFonts.sourceCodePro(
+                        color: col2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100)),
+              ],
+              isRepeatingAnimation: false,
+            )
+          ],
+        )));
+  }
 }
