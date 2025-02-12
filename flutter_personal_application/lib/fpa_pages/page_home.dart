8e47ef038454156d66af51870819abb34a227d9d
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 09:01:40 +0530

    Home Page Styling Typewriter plus sequential, looks good, still can be upgraded miles above

--- flutter_personal_application/lib/fpa_pages/page_home.dart
+++ flutter_personal_application/lib/fpa_pages/page_home.dart
@@ -13,6 +13,7 @@ class _PageHome extends State<PageHome>
     Widget build(BuildContext context)
     {
         return Container(
+          padding: EdgeInsets.all(13.0),
           decoration: BoxDecoration(gradient: LinearGradient(colors:col)),
             child: Center(child:Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -20,7 +21,10 @@ class _PageHome extends State<PageHome>
                 children: [
                 AnimatedTextKit(
                   animatedTexts: [
-                    TyperAnimatedText('Hello Hey There Testing My TypeWriter Effect',textStyle: TextStyle(color: col2,fontWeight: FontWeight.bold,fontSize: 20,overflow: TextOverflow.clip,fontFamily: ''))
+                    TyperAnimatedText('Ideate.',textStyle: TextStyle(color: col2,fontWeight: FontWeight.bold,fontSize: 70,overflow: TextOverflow.clip,fontFamily: 'Agne',)),
+                    TyperAnimatedText('Create.',textStyle: TextStyle(color: col2,fontWeight: FontWeight.bold,fontSize: 70,overflow: TextOverflow.clip,fontFamily: 'Agne',)),
+                    TyperAnimatedText('Revolutionize.',textStyle: TextStyle(color: col2,fontWeight: FontWeight.bold,fontSize: 70,overflow: TextOverflow.clip,fontFamily: 'Agne',)),
+
                   ],
                   isRepeatingAnimation: false,
                 )
