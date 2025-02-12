38140b5db6e3745c7752c88e1c9dfb3a824c2386
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 08:38:13 +0530

    terminal style effects going good, found a great library, flutter animated text

--- flutter_personal_application/lib/fpa_pages/page_home.dart
+++ flutter_personal_application/lib/fpa_pages/page_home.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
 class PageHome extends StatefulWidget
@@ -13,6 +14,18 @@ class _PageHome extends State<PageHome>
     {
         return Container(
           decoration: BoxDecoration(gradient: LinearGradient(colors:col)),
+            child: Center(child:Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TyperAnimatedText('Hello Hey There Testing My TypeWriter Effect',textStyle: TextStyle(color: col2,fontWeight: FontWeight.bold,fontSize: 20,overflow: TextOverflow.clip,fontFamily: ''))
+                  ],
+                  isRepeatingAnimation: false,
+                )
+                ],
+            ))
         );
     }
 }
--- flutter_personal_application/lib/global_theme/global_theme.dart
+++ flutter_personal_application/lib/global_theme/global_theme.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 //frost
 List<Color> col = [Color(0xff020202),Color(0xff020202)];
 var stl = TextStyle(color: Colors.white,fontWeight: FontWeight.bold);
+Color col2 = Colors.green;
