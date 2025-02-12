dade5bbf52a542b6bb12df631f2fc8b84c24b19b
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 11:16:05 +0530

    global theme.dart now has test gradients in it to save time and effort for future navigation testing

--- flutter_personal_application/lib/global_theme/global_theme.dart
+++ flutter_personal_application/lib/global_theme/global_theme.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 List<Color> col = [Color(0xff020202),Color(0xff020202)];
 var stl = TextStyle(color: Colors.white,fontWeight: FontWeight.bold);
 Color col2 = Colors.white;
+List<Color> test1 = [Colors.green,Colors.yellow];
+List<Color> test2 = [Colors.red,Colors.blue];
+List<Color> test3 = [Colors.purple,Colors.black];
+List<Color> test4 = [Colors.brown,Colors.cyan];
