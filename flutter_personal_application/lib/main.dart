ade124939b4e082f65066c2c3e43886f259e1ffa
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 22:13:28 +0530

    Version-1/5: HomePage/AboutPage Completed, 4 more pages to go, Final Directory Structure Established

--- flutter_personal_application/lib/main.dart
+++ flutter_personal_application/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_personal_application/main_view_control.dart';
+import 'package:flutter_personal_application/view_control/main_view_control.dart';
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MainViewController()));
 }
