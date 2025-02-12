e011725588f3d51428425e6fb6c3a48a9b4a37f8
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-12 11:43:59 +0530

    setting up educationPage

--- flutter_personal_application/lib/fpa_pages/page_education.dart
+++ flutter_personal_application/lib/fpa_pages/page_education.dart
@@ -1,16 +1,25 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
-class PageEducation extends StatefulWidget
-{
-    const PageEducation({super.key});
-    @override
-    State<PageEducation> createState() => _PageEducation();
+
+class PageEducation extends StatefulWidget {
+  const PageEducation({super.key});
+  @override
+  State<PageEducation> createState() => _PageEducation();
 }
-class _PageEducation extends State<PageEducation>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Container(decoration: BoxDecoration(gradient: LinearGradient(colors: col)),);
-    }
+
+class _PageEducation extends State<PageEducation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+      child: Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
 }
