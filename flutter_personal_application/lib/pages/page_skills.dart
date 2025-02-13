cfda0c76de6e5f7763184387293c59cbbb5a6d8b
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-13 22:49:09 +0530

    Very poor documentation quality for roulette library replaced it with flutter_fortune_wheel

--- flutter_personal_application/lib/pages/page_skills.dart
+++ flutter_personal_application/lib/pages/page_skills.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
-import 'package:roulette/roulette.dart';
-class PageSkills extends StatefulWidget
-{
-    const PageSkills({super.key});
-    @override
-    State<PageSkills> createState() => _PageSkills();
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+class PageSkills extends StatefulWidget {
+  const PageSkills({super.key});
+  @override
+  State<PageSkills> createState() => _PageSkills();
 }
-class _PageSkills extends State<PageSkills>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Container(decoration: BoxDecoration(gradient: LinearGradient(colors: col)),);
-    }
+
+class _PageSkills extends State<PageSkills> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+      child: Column()
+    );
+  }
 }
