16f58bab44c5ab419602262c392af4cbba5480a8
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-13 10:50:58 +0530

    Version-2/5: Social Links Completed

--- flutter_personal_application/lib/pages/page_home.dart
+++ flutter_personal_application/lib/pages/page_home.dart
@@ -3,13 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 class PageHome extends StatefulWidget {
   const PageHome({super.key});
   @override
   State<PageHome> createState() => _PageHome();
 }
 
-class _PageHome extends State<PageHome> {
+class _PageHome extends State<PageHome> 
+{
+  final Uri _url1 = Uri.parse('https://github.com/perpetualcuriosity');
+  Future<void> _launchUrl1() async {
+  if (!await launchUrl(_url1)) {
+    throw Exception('Could not launch $_url1');
+  }
+  }
+  final Uri _url2 = Uri.parse('https://www.linkedin.com/in/jashupadhyay/');
+  Future<void> _launchUrl2() async {
+  if (!await launchUrl(_url2)) {
+    throw Exception('Could not launch $_url2');
+  }
+  }
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -35,7 +49,7 @@ class _PageHome extends State<PageHome> {
           child: Row(
             children: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {_launchUrl1();},
                 icon: Image.network(
                   'https://img.icons8.com/?size=100&id=u9R54eMKS8fw&format=png&color=000000',
                   width: 50,
@@ -43,17 +57,9 @@ class _PageHome extends State<PageHome> {
                 ),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {_launchUrl2();},
                 icon: Image.network(
                   'https://img.icons8.com/?size=100&id=114445&format=png&color=000000',
-                  width: 50,
-                  height: 50,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Image.network(
-                  'https://img.icons8.com/?size=100&id=Cjuj2uISMdQ1&format=png&color=000000',
                   width: 50,
                   height: 50,
                 ),
