44d30ea73953b0dfc160743a33b349e3f925b436
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-14 00:45:23 +0530

    Version-3/5: Skills Page Completed, Responsiveness Yet To Be Added

--- flutter_personal_application/lib/pages/page_skills.dart
+++ flutter_personal_application/lib/pages/page_skills.dart
@@ -2,65 +2,107 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
+
 class PageSkills extends StatefulWidget {
   const PageSkills({super.key});
+
   @override
   State<PageSkills> createState() => _PageSkills();
 }
 
 class _PageSkills extends State<PageSkills> {
   StreamController<int> controller = StreamController<int>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
-        child: Center(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.all(40),
+          margin: EdgeInsets.symmetric(horizontal: 80, vertical: 40),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.6),
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Colors.white, width: 3),
+          ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  SizedBox(
-                    height: 550,
-                    width: 550,
-                    child: FortuneWheel(
-                      selected: controller.stream,
-                      items: [
-                        FortuneItem(child: Image.network('https://img.icons8.com/?size=100&id=PmGdjRbnOudE&format=png&color=000000')),
-                        FortuneItem(child: Image.network('https://img.icons8.com/?size=100&id=YX03OUiHE3rz&format=png&color=000000')),
-                        FortuneItem(child: Image.network('https://img.icons8.com/?size=100&id=39853&format=png&color=000000')),
-                      ],
+              SizedBox(
+                height: 450,
+                width: 450,
+                child: FortuneWheel(
+                  selected: controller.stream,
+                  items: [
+                    FortuneItem(
+                      child: Image.network(
+                        'https://img.icons8.com/?size=100&id=PmGdjRbnOudE&format=png&color=000000',
+                        height: 70,
+                      ),
                     ),
-                  )
-                ],
-              )),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                      AnimatedTextKit(animatedTexts: [
-                          TypewriterAnimatedText('My Skills',speed:Duration(milliseconds: 100),textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50))
-                      ]),
-                      AnimatedTextKit(animatedTexts: [
-                          TypewriterAnimatedText('Flutter',speed:Duration(milliseconds: 100),textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50))
-                      ]),
-                      AnimatedTextKit(animatedTexts: [
-                        TypewriterAnimatedText('Python',speed:Duration(milliseconds: 100),textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50))
-                      ]),
-                      AnimatedTextKit(animatedTexts: [
-                        TypewriterAnimatedText('Javascript',speed:Duration(milliseconds: 100),textStyle: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 50))
-                      ])
+                    FortuneItem(
+                      child: Image.network(
+                        'https://img.icons8.com/?size=100&id=YX03OUiHE3rz&format=png&color=000000',
+                        height: 70,
+                      ),
+                    ),
+                    FortuneItem(
+                      child: Image.network(
+                        'https://img.icons8.com/?size=100&id=39853&format=png&color=000000',
+                        height: 70,
+                      ),
+                    ),
                   ],
                 ),
-              )
+              ),
+              SizedBox(width: 70),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'My Skills',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    'Flutter',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Python',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'JavaScript',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
