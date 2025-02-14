1ec15c667c506bf888532e095dba07fba62da741
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-14 08:54:07 +0530

    projects page basic strucutre established

--- flutter_personal_application/lib/pages/page_projects.dart
+++ flutter_personal_application/lib/pages/page_projects.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
-class PageProjects extends StatefulWidget
-{
-    const PageProjects({super.key});
-    @override
-    State<PageProjects> createState() => _PageProjects();
+
+class PageProjects extends StatefulWidget {
+  const PageProjects({super.key});
+  @override
+  State<PageProjects> createState() => _PageProjects();
 }
-class _PageProjects extends State<PageProjects>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Container(decoration: BoxDecoration(gradient: LinearGradient(colors: col)),);
-    }
+
+class _PageProjects extends State<PageProjects> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+      child: Container(
+        padding: EdgeInsets.all(40),
+        margin: EdgeInsets.symmetric(horizontal: 80, vertical: 40),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(
+              width: 3,
+              style: BorderStyle.solid,
+              color: Colors.white,
+            )),
+            child: Column(
+                children: [
+                    
+                ],
+            ),
+      ),
+    );
+  }
 }
