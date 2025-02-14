5a3075dc9a88ff7ad8321e1a02c596a7f0b97382
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-14 09:01:41 +0530

    home page made cohesive with rest of website

--- flutter_personal_application/lib/pages/page_home.dart
+++ flutter_personal_application/lib/pages/page_home.dart
@@ -1,117 +1,124 @@
+import 'dart:async';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter_personal_application/global_theme/global_theme.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 class PageHome extends StatefulWidget {
   const PageHome({super.key});
+
   @override
   State<PageHome> createState() => _PageHome();
 }
 
-class _PageHome extends State<PageHome> 
-{
+class _PageHome extends State<PageHome> {
   final Uri _url1 = Uri.parse('https://github.com/perpetualcuriosity');
-  Future<void> _launchUrl1() async {
-  if (!await launchUrl(_url1)) {
-    throw Exception('Could not launch $_url1');
-  }
-  }
   final Uri _url2 = Uri.parse('https://www.linkedin.com/in/jashupadhyay/');
-  Future<void> _launchUrl2() async {
-  if (!await launchUrl(_url2)) {
-    throw Exception('Could not launch $_url2');
+  final Uri _url3 = Uri.parse('https://www.linkedin.com/in/jashupadhyay/');
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
   }
-  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Image.asset(
-            'lib/assets/cbg.jpg',
-            fit: BoxFit.cover,
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.all(40),
+          margin: EdgeInsets.symmetric(horizontal: 80, vertical: 40),
+          decoration: BoxDecoration(
+            color: Color(0xff020202),
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Colors.white, width: 3),
           ),
-        ),
-        Positioned.fill(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            child: Container(
-              // ignore: deprecated_member_use
-              color: Colors.black.withOpacity(0.55),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 20,
-          right: 20,
-          child: Row(
+          child: Stack(
             children: [
-              IconButton(
-                onPressed: () {_launchUrl1();},
-                icon: Image.network(
-                  'https://img.icons8.com/?size=100&id=u9R54eMKS8fw&format=png&color=000000',
-                  width: 50,
-                  height: 50,
+              Positioned(
+                top: 20,
+                right: 20,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () => _launchUrl(_url1),
+                      icon: Image.network(
+                        'https://img.icons8.com/?size=100&id=u9R54eMKS8fw&format=png&color=ffffff',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () => _launchUrl(_url2),
+                      icon: Image.network(
+                        'https://img.icons8.com/?size=100&id=114445&format=png&color=ffffff',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () => _launchUrl(_url3),
+                      icon: Image.network(
+                        'https://img.icons8.com/?size=100&id=EgRndDDLh8kS&format=png&color=000000',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              IconButton(
-                onPressed: () {_launchUrl2();},
-                icon: Image.network(
-                  'https://img.icons8.com/?size=100&id=114445&format=png&color=000000',
-                  width: 50,
-                  height: 50,
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Jash Upadhyay',
+                      style: GoogleFonts.sourceCodePro(
+                        color: col2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText('Solve.',
+                            speed: Duration(milliseconds: 150),
+                            textStyle: GoogleFonts.sourceCodePro(
+                                color: col2,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40)),
+                        TypewriterAnimatedText('Code.',
+                            speed: Duration(milliseconds: 150),
+                            textStyle: GoogleFonts.sourceCodePro(
+                                color: col2,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40)),
+                        TypewriterAnimatedText('Transform.',
+                            speed: Duration(milliseconds: 150),
+                            textStyle: GoogleFonts.sourceCodePro(
+                                color: col2,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40)),
+                        TypewriterAnimatedText('Flutter Developer',
+                            speed: Duration(milliseconds: 150),
+                            textStyle: GoogleFonts.sourceCodePro(
+                                color: col2,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40)),
+                      ],
+                      isRepeatingAnimation: false,
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
         ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Jash Upadhyay',
-                style: GoogleFonts.sourceCodePro(
-                  color: col2,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 100,
-                ),
-              ),
-              AnimatedTextKit(
-                animatedTexts: [
-                  TypewriterAnimatedText('Solve.',
-                      speed: Duration(milliseconds: 150),
-                      textStyle: GoogleFonts.sourceCodePro(
-                          color: col2,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 100)),
-                  TypewriterAnimatedText('Code.',
-                      speed: Duration(milliseconds: 150),
-                      textStyle: GoogleFonts.sourceCodePro(
-                          color: col2,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 100)),
-                  TypewriterAnimatedText('Transform.',
-                      speed: Duration(milliseconds: 150),
-                      textStyle: GoogleFonts.sourceCodePro(
-                          color: col2,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 100)),
-                  TypewriterAnimatedText('Flutter Developer',
-                      speed: Duration(milliseconds: 150),
-                      textStyle: GoogleFonts.sourceCodePro(
-                          color: col2,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 100)),
-                ],
-                isRepeatingAnimation: false,
-              ),
-            ],
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
