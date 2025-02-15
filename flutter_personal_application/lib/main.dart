3e2207d1182e657726de3a658e9d41c8899f3479
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-15 08:48:18 +0530

    TitleUpdate In Material App

--- flutter_personal_application/lib/main.dart
+++ flutter_personal_application/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/view_control/main_view_control.dart';
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MainViewController()));
+  runApp(MaterialApp(title: 'Jash Upadhyay',debugShowCheckedModeBanner: false, home: MainViewController()));
 }
