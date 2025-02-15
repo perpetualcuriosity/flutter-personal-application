91366dc5d2d937a02bc46c360994a95feb463696
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-02-15 09:16:17 +0530

    Application Completed, Final Devlopment Commit, All New Commits From Now On Will Be To Add To Skills/Projects

--- flutter_personal_application/lib/main.dart
+++ flutter_personal_application/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_personal_application/view_control/main_view_control.dart';
+
 void main() {
-  runApp(MaterialApp(title: 'Jash Upadhyay',debugShowCheckedModeBanner: false, home: MainViewController()));
+  runApp(MaterialApp(
+      title: 'Jash Upadhyay',
+      debugShowCheckedModeBanner: false,
+      home: MainViewController()));
 }
